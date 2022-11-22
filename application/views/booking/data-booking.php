a211075ba375d81e3db21f1b9cb05aca23e483ca
Alifiandi000 <alifiandi2000@gmail.com>
2022-11-22 15:18:29 +0700

    first commit

---
+++ application/views/booking/data-booking.php
@@ -0,0 +1,21 @@
+<div class="container">
+  <center>
+    <table>
+      <tr>
+        <td>
+          <div class="table-responsive full-width">
+            <table class="table table-bordered table-stripped table-hover" id="table-datatable">
+              <tr>
+                <th>No.</th>
+                <th>Buku</th>
+                <th>Penulis</th>
+                <th>Penerbit</th>
+                <th>Tahun</th>
+                <th>Pilihan</th>
+              </tr>
+              <?php
+              $no = 1;
+              foreach ($temp as $t) {
+              ?>
+              
+              }
